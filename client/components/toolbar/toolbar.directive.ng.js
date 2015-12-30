7004314c5495f282fecd3d0bd6a806bb9c38dfd3
Guillaume BORG <guillaume.unice@gmail.com>
2015-12-30 15:48:11 +0100

    first commit

---
+++ client/components/toolbar/toolbar.directive.ng.js
@@ -0,0 +1,10 @@
+'use strict'
+
+angular.module('testYoApp')
+.directive('toolbar', function() {
+  return {
+    restrict: 'AE',
+    templateUrl: 'client/components/toolbar/toolbar.view.ng.html',
+    replace: true
+  };
+});
